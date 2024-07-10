9575cdb419c5629c9fbdc0a681f2ff7939f42df8
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-07-10 23:14:35 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center">
+      <header className=" text-black p-4">
+        <h1 className="text-6xl font-bold">Hello World!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
