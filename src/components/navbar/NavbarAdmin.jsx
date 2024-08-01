9262f61864944114243fb351ea3bcac73726da7f
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-08-01 16:20:30 +0700

    logout navigate

--- src/components/navbar/NavbarAdmin.jsx
+++ src/components/navbar/NavbarAdmin.jsx
@@ -89,7 +89,13 @@ const NavbarAdmin = ({ children }) => {
 
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
-    navigate("/admin/login");
+    if (role === "admin") {
+      navigate("/admin/login");
+    } else if (role === "nakes") {
+      navigate("/puskesmas/login");
+    } else if (role === "apoteker") {
+      navigate("/apotek/login");
+    }
   };
 
   const Sidebar = () => (
