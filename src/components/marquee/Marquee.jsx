59f489dc0ae79c9527b9edfd786407f11c1b4755
scrkiddie <hilmitrial4@gmail.com>
2024-11-23 23:56:28 +0700

    chore: memperbaiki tampilan

--- src/components/marquee/Marquee.jsx
+++ src/components/marquee/Marquee.jsx
@@ -1,13 +1,7 @@
 import ReactMarquee from "react-fast-marquee";
-import { useNavigate } from "react-router-dom";
-import { Button } from "primereact/button";
 
 const Marquee = ({ data }) => {
-  const navigate = useNavigate();
 
-  const handleClickButton = () => {
-    navigate("/prolanis");
-  };
 
   return (
     <div className="w-full flex flex-col md:flex-row gap-2 ">
@@ -15,10 +9,10 @@ const Marquee = ({ data }) => {
         {data.length > 0 ? (
           data.map((item, index) => (
             <div className="text-darkColor dark:text-whiteHover" key={index}>
-              <h1 className="text-md ">
-                {" "}
-                | Jadwal Prolanis: {item.waktuMulai} - {item.waktuSelesai} di{" "}
-                {item.adminPuskesmas.namaPuskesmas}{" "}
+              <h1 className="text-md">
+                  {" "}
+                  ‎ 📢 {item.adminPuskesmas.namaPuskesmas}: Jadwal prolanis mulai {item.waktuMulai} hingga {item.waktuSelesai + " "}
+                  {" "}
               </h1>
             </div>
           ))
@@ -26,16 +20,6 @@ const Marquee = ({ data }) => {
           <div>Belum ada jadwal prolanis</div>
         )}
       </ReactMarquee>
-
-      {data.length > 0 ? (
-        <Button
-          size="small"
-          className="bg-mainGreen px-3 flex items-center justify-center"
-          onClick={handleClickButton}
-        >
-          Selengkapnya
-        </Button>
-      ) : null}
     </div>
   );
 };
