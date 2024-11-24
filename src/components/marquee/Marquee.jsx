e5665533de822ada05d0b6ba45603fc31e4c6c9e
scrkiddie <hilmitrial4@gmail.com>
2024-11-24 10:23:01 +0700

    chore: ganti running text

--- src/components/marquee/Marquee.jsx
+++ src/components/marquee/Marquee.jsx
@@ -4,21 +4,17 @@ const Marquee = ({ data }) => {
 
 
   return (
-    <div className="w-full flex flex-col md:flex-row gap-2 ">
-      <ReactMarquee gradient={false} speed={50} className="border-y-[3px] py-2">
-        {data.length > 0 ? (
-          data.map((item, index) => (
-            <div className="text-darkColor dark:text-whiteHover" key={index}>
-              <h1 className="text-md">
-                  {" "}
-                  ‎ 📢 {item.adminPuskesmas.namaPuskesmas}: Jadwal prolanis mulai {item.waktuMulai} hingga {item.waktuSelesai + " "}
-                  {" "}
-              </h1>
-            </div>
-          ))
-        ) : (
-          <div>Belum ada jadwal prolanis</div>
-        )}
+    <div className={`w-full flex-col md:flex-row gap-2 ${data.length > 0 ? "flex":"hidden"}` }>
+      <ReactMarquee gradient={false} speed={50} className={`border-y-[3px] py-2`}>
+          {data.map((item, index) => (
+              <div className="text-darkColor dark:text-whiteHover" key={index}>
+                  <h1 className="text-md">
+                      {" "}
+                      ‎ 📢 Jadwal prolanis {item.adminPuskesmas.namaPuskesmas} mulai {item.waktuMulai} hingga {item.waktuSelesai + " "}
+                      {" "}
+                  </h1>
+              </div>
+          ))}
       </ReactMarquee>
     </div>
   );
